c4d0c16df96222e0b66b0c00e467653d9bc5eefa
Dylan Caron <dcaron81.dc@gmail.com>
2021-02-03 15:10:40 +0100

    Ajout du script de déplacement pour faire bouger un cube. Certains comportement 'bizarres' sont à corriger

--- UnithonUnity/Assets/Scripts/Deplacer.cs
+++ UnithonUnity/Assets/Scripts/Deplacer.cs
@@ -1,33 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Deplacer : MonoBehaviour
 {
+    private static float DELTA_POS = 0.01F;
+
+    public float x, y, z;
+    private Vector3 dest;
+
     // Start is called before the first frame update
     void Start()
     {
-        deplacer (new Vector3 (0, 0, 0));
+        dest = new Vector3 (x, y, z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Deplacement
+        if (Math.Abs (transform.position.x - dest.x) > DELTA_POS ||
+            Math.Abs (transform.position.z - dest.z) > DELTA_POS)
+        {
+            deplacer (dest);
+        }
     }
 
     public void deplacer (Vector3 dest)
     {
-        float dist_x = transform.position.x - dest.x;
-        float dist_z = transform.position.z - dest.z;
+        float dist_x = dest.x - transform.position.x;
+        float dist_z = dest.z - transform.position.z;
 
-        while (transform.position != dest)
-        {
-            transform.position = transform.position + new Vector3 (
-                transform.position.x + dist_x / (dist_x + dist_z),
-                transform.position.y,
-                transform.position.z + dist_z / (dist_x + dist_z)
-            ) * Time.deltaTime;
-        }      
+        transform.Translate (new Vector3 (
+            dist_x / Math.Abs (dist_x + dist_z),
+            0,
+            dist_z / Math.Abs (dist_x + dist_z)
+        ) * Time.deltaTime * 4);
     }
 }
