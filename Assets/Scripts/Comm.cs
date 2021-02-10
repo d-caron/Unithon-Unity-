5cb0553c764f2e5ce98150ff63fbc20b60dbfb7f
Dylan Caron <dcaron81.dc@gmail.com>
2021-02-10 15:18:08 +0100

    + Toute la communication se fait dorénavant en TCP et dans le script Comm.cs + L'écoute de message entrant est désormais sur un thread a part.

---
+++ Assets/Scripts/Comm.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Comm : MonoBehaviour
+{
+    private TcpClient socketConnection;
+    private Thread listenThread;
+    
+    public string host = "localhost";
+    public int port;
+    public string message;
+
+    private void Start ()
+    {
+        ConnectToTCPServer ();
+        listenThread = new Thread (new ThreadStart (ListenForData));
+        listenThread.Start();
+    }
+
+    private void ConnectToTCPServer ()
+    {
+        try
+        {
+            socketConnection = new TcpClient (host, port);
+        }
+        catch (Exception)
+        {
+            throw new Exception ("Server not running");
+        }
+    }
+
+    public void SendTCPMessage () {
+        try
+        {
+            NetworkStream stream = socketConnection.GetStream ();
+            if (stream.CanWrite)
+            {
+                byte[] messageAsByteArray = Encoding.ASCII.GetBytes (message);
+                stream.Write (messageAsByteArray, 0, messageAsByteArray.Length);
+                Debug.Log ("Ca marche !");
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log ("Exception : " + e);
+        }
+    }
+
+    private void ListenForData ()
+    {
+        try
+        {
+            byte[] bytes = new byte[2048];
+
+            using (NetworkStream stream = socketConnection.GetStream ())
+            {
+                int length;
+                while ((length = stream.Read (bytes, 0, bytes.Length)) != 0)
+                {
+                    Debug.Log (Encoding.ASCII.GetString (bytes));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log ("Exception : " + e);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        CloseTCPClient ();
+    }
+
+    public void CloseTCPClient()
+    {
+        socketConnection.Close ();
+        Debug.Log ("Connexion closed !");
+    }
+}
