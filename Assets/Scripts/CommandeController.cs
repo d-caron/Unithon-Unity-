2469590ae0223f5ec4ac545dbbd168fff24758aa
Raphael Huc <raph82130@gmail.com>
2021-03-10 16:15:16 +0100

    Fix merge + suppression d'un debug.log inutile

--- Assets/Scripts/CommandeController.cs
+++ Assets/Scripts/CommandeController.cs
@@ -65,7 +65,6 @@ public class CommandeController : MonoBehaviour
         // On regarde si il y a une commande en attente pour le personnage dont le nom est "id" (nom de l'objet)
         
         Commande cmd = commands.Find(commands => commands.ids[0].Equals(id));
-        Debug.Log(cmd);
         // Si on trouve une commande, on affecte alors au personnage cette commande (la première qui trouve dans la liste)
         if (cmd != null) {
             GameObject.Find(id).GetComponent<CharacterControl>().SetCommand(cmd);
